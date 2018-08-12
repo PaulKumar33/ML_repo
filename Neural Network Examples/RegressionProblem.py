05ad09084330f7aa9b4869be49b7b8995292d41c
PaulBucket33 <paul.kumar@mail.utoronto.ca>
2018-08-11 23:03:36 -0400

    committing

--- Neural Network Examples/RegressionProblem.py
+++ Neural Network Examples/RegressionProblem.py
@@ -53,7 +53,17 @@ for i in range(k):
 
 import matplotlib.pyplot as plt
 
-plt.plot(range(1, len(average_mae_history) + 1), average_mae_history)
+def smooth_curve(points, factor=0.9):
+    smoothed_points = []
+    for point in points:
+        if smoothed_points:
+            previous = smoothed_points[-1]
+            smoothed_points.append(previous * factor + point * (1 - factor))
+        else:
+            smoothed_points.append(point)
+    return smoothed_points
+smooth_mae_history = smooth_curve(average_mae_history[10:])
+plt.plot(range(1, len(smooth_mae_history) + 1), smooth_mae_history)
 plt.xlabel('Epochs')
 plt.ylabel('Validation MAE')
 plt.show()
